4f28c9c21f75a3ac68935e4d8df4bd69adab3329
hs2041 <hsingh1097@gmail.com>
2022-07-22 20:19:58 +0530

    base set

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hardeep/repos/cuda_learner
-# Build directory: /home/hardeep/repos/cuda_learner/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
