ec3404fcc18fe3c2f3342ef14f1366ead293b671
hs2041 <hsingh1097@gmail.com>
2022-07-22 20:19:16 +0530

    base set

---
+++ build/CMakeFiles/cuda_lessons.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_lessons.dir/main.cpp.o"
+  "cuda_lessons"
+  "cuda_lessons.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cuda_lessons.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hardeep/repos/cuda_learner
+# Build directory: /home/hardeep/repos/cuda_learner/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
